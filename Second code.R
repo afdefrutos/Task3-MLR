5a8dddf864f7ce2314b8fe1a56e1772ddc280cc6
Andres de Frutos <afdefrutos@gmail.com>
2018-08-28 15:36:29 +0200

    Correlation Matrix

--- Second code.R
+++ Second code.R
@@ -1,28 +1,44 @@
 library(readr)
 setwd("~/Desktop/Task-3---Multiple-Regression")
-library(readr)
-LucasAndres <- read_csv("LucasAndres.csv")
+LucasAndres <- read_csv("~/Desktop/Task3-MLR/LucasAndres.csv")
 View(LucasAndres)
 
 
-LucasAndres$Dimension <- LucasAndres$Depth*LucasAndres$Width*LucasAndres$Heigth
+#LucasAndres$Dimension <- LucasAndres$Depth*LucasAndres$Width*LucasAndres$Heigth
 LucasAndres$Depth <- NULL
 LucasAndres$Width <- NULL
 LucasAndres$Heigth <- NULL
 LucasAndres$X1 <- NULL
 LucasAndres$Best_seller_rank <- NULL
 LucasAndres$`1Stars` <- NULL
+LucasAndres$`5Stars` <- NULL
+LucasAndres$`3Stars` <- NULL
+LucasAndres$`2Stars`<- NULL
+LucasAndres$Environment_Impact <- NULL
+
+LucasAndres$Relative_Price <- NULL
+LucasAndres$Product_ID <- NULL
+LucasAndres$Dimension <- NULL
+
+#LucasAndres$Dimension[is.na(LucasAndres$Dimension)] <- round(mean(LucasAndres$Dimension, na.rm = TRUE))
 
 
 LucasAndresDV<- dummyVars(" ~ .", data = LucasAndres)
 LucasAndresDVready <- data.frame(predict(LucasAndresDV, newdata = LucasAndres))
 
 
-LucasAndresDVready$Dimension <- NULL
 
 
-corrData <- cor(LucasAndresDVready)
+
+try <- LucasAndres
+
+try$Product_type<- NULL
+
+corrData <- cor(try)
+
 corrData
 
-corrplot(corrData)
- 
+corrplot(corrData, method="circle")
+corrData
+
+dev.off()
